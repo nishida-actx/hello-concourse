30322debc646781fa4d52f95f2960cbeb7ce96df
nishida-act <nishida@act-x.co.jp>
2017-03-14 11:54:47 +0900

    update pipeline2

--- commit-logs.sh
+++ commit-logs.sh
@@ -7,8 +7,8 @@ cd updated-result/
 # 前Task出力結果をGitのcommit用のGit作業ディレクトリに移動する
 mv -f ../out/* ./
 
-git config --global user.email "nishidax at act-x dot co dot jp"
-git config --global user.name "nishidat"
+git config --global user.email "nishida at act-x dot co dot jp"
+git config --global user.name "nishida-actx"
 
 git add -A
 git commit -m "Update result log"
